1dfe7a49db629dbc7487233dd7f00f072101be65
stevenremenapp <remenapp@gmail.com>
2019-05-02 19:05:41 -0400

    Advance in TypeScript; add changes from old courses

--- typeScript/maxCourse/ex1/script.js
+++ typeScript/maxCourse/ex1/script.js
@@ -1,16 +1,78 @@
-var Greeter = /** @class */ (function () {
-    function Greeter(message) {
-        this.greeting = message;
-    }
-    Greeter.prototype.greet = function () {
-        return "Hello, " + this.greeting;
-    };
-    return Greeter;
-}());
-var greeter = new Greeter("world");
-var button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function () {
-    alert(greeter.greet());
+"use strict";
+var myName = 'Steve'; // Implicit definition
+var age; // Explicit definition
+age = 31;
+var hobbies = ["Running", "Reading"];
+console.log(hobbies);
+hobbies = [100];
+console.log(hobbies);
+//tuples
+var address = ["Superstreet", 999];
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 101
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+//functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+//void function type does not return anything
+function sayHello() {
+    console.log("Hello!");
+}
+//argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+//function types
+var myMultiply;
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+//objects
+var userData = {
+    name: "Steve",
+    age: 31
 };
-document.body.appendChild(button);
+//complex object
+var complex = {
+    data: [100, 3.99, 19],
+    output: function (argument) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [1, 2, 3],
+    output: function (argument) {
+        return this.data;
+    }
+};
+//union types
+var myRealAge = 31;
+myRealAge = '31';
+//check types
+var finalValue = 23;
+if (typeof finalValue == "number") {
+    console.log("Final value is a number!");
+}
+// class Greeter {
+//     greeting: string;
+//     constructor(message: string) {
+//         this.greeting = message;
+//     }
+//     greet() {
+//         return "Hello, " + this.greeting;
+//     }
+// }
+// let greeter = new Greeter("world");
+// let button = document.createElement('button');
+// button.textContent = "Say Hello";
+// button.onclick = function() {
+//     alert(greeter.greet());
+// }
+// document.body.appendChild(button);
--- typeScript/maxCourse/ex1/script.ts
+++ typeScript/maxCourse/ex1/script.ts
@@ -1,19 +1,100 @@
-class Greeter {
-    greeting: string;
-    constructor(message: string) {
-        this.greeting = message;
-    }
-    greet() {
-        return "Hello, " + this.greeting;
-    }
+let myName = 'Steve'; // Implicit definition
+let age: number; // Explicit definition
+age = 31;
+
+let hobbies: any[] = ["Running", "Reading"];
+console.log(hobbies);
+hobbies = [100];
+console.log(hobbies);
+
+//tuples
+let address: [string, number] = ["Superstreet", 999];
+
+//enum
+enum Color {
+    Gray, // 0
+    Green = 100, // 100 -- if num is specified, the count continues by adding one to the previous; can be reset if next item is set to 2
+    Blue // 101
 }
 
-let greeter = new Greeter("world");
+let myColor: Color = Color.Green;
+console.log(myColor);
 
-let button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function() {
-    alert(greeter.greet());
+//functions
+function returnMyName(): string {
+    return myName;
 }
 
-document.body.appendChild(button);
+console.log(returnMyName());
+
+//void function type does not return anything
+function sayHello(): void {
+    console.log("Hello!");
+}
+
+//argument types
+function multiply(value1: number, value2: number) {
+    return value1 * value2;
+}
+
+//function types
+let myMultiply: (val1: number, val2: number) => number;
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
+
+//objects
+let userData: { name: string, age: number } = {
+    name: "Steve",
+    age: 31
+};
+
+//complex object
+let complex: {data: number[], output: (argument: boolean) => number[]} = {
+    data: [100, 3.99, 19],
+    output: function (argument: boolean): number[] {
+        return this.data;
+    }
+};
+
+//type alias - used to make a template for complex types/types to be reused
+type Complex = {data: number[], output: (argument: boolean) => number[]};
+
+let complex2: Complex = {
+    data: [1, 2, 3],
+    output: function (argument: boolean): number[] {
+        return this.data;
+    }
+};
+
+//union types
+let myRealAge: number | string = 31;
+myRealAge = '31'
+
+//check types
+let finalValue = 23;
+if (typeof finalValue == "number") {
+    console.log("Final value is a number!");
+}
+
+
+
+
+// class Greeter {
+//     greeting: string;
+//     constructor(message: string) {
+//         this.greeting = message;
+//     }
+//     greet() {
+//         return "Hello, " + this.greeting;
+//     }
+// }
+
+// let greeter = new Greeter("world");
+
+// let button = document.createElement('button');
+// button.textContent = "Say Hello";
+// button.onclick = function() {
+//     alert(greeter.greet());
+// }
+
+// document.body.appendChild(button);
