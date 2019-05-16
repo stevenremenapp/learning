481cfea5f637fff14146effa9d657ea2c2552c59
stevenremenapp <remenapp@gmail.com>
2019-05-15 20:57:32 -0400

    Start C# course

---
+++ cSharp/udemyMasterclass/helloWorld/helloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static sbyte number = 15;
+        static void Main(string[] args)
+        {
+            number = 20;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+            Console.WriteLine(number);
+            Console.WriteLine("hello world");
+            Console.WriteLine("Hello Steve");
+            Console.Read();
+        }
+    }
+}
