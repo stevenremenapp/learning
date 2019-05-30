4473409906024d3ad6c0a4a6c4b731f3af2b1ce6
stevenremenapp <remenapp@gmail.com>
2019-05-29 20:58:22 -0400

    Work through C# members and properties part of class

---
+++ cSharp/udemyMasterclass/MembersFinalizersDestructors/MembersFinalizersDestructors/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MembersFinalizersDestructors
+{
+    class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary = 20000;
+
+        // member - public field
+        public int age;
+
+        // member - property - exposes jobTitle safely
+        public string JobTitle {
+            get
+            {
+                return jobTitle;
+            }
+            set
+            {
+                jobTitle = value;
+            }
+        }
+
+        // public member method
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine($"Hi, my name is {memberName}, I'm {age} years old, and my job title is {jobTitle}.");
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine($"My salary is {salary}.");
+        }
+
+        // member constructor
+        public Members()
+        {
+            age = 31;
+            memberName = "Steve";
+            salary = 30000;
+            jobTitle = "software development intern";
+            Console.WriteLine("Object created.");
+        }
+
+        // member - finalizer - destructor
+        // only one destructor per class
+        // finalizer cannot be overloaded
+        // Whenever the object runs out of scope this is called
+        ~Members()
+        {
+            // cleanup statements
+            Console.WriteLine("Deconstruction of Members object.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(true);
+            Debug.Write("Destruction of Members object!");
+        }
+    }
+}
---
+++ cSharp/udemyMasterclass/Properties/Properties/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace Properties
+{
+    class Box
+    {
+        // Constructor
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.Width = width;
+            this.height = height;
+        }
+
+        // member variables
+        private int length;
+        private int height;
+        // private int width;
+        private int volume;
+
+        // CAN MAKE PROPERTIES READ OR WRITE ONLY IF GET OR SET IS ONLY SET
+
+        // Long way to use getter and setter with Property
+        /*
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                length = value;
+            }
+        }
+        */
+        
+        // Using => Operator to get and set
+        public int Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        // Short way to write get/set -- type Prop and tab twice
+        // Auto-implemented property
+        public int Width { get; set; }
+
+        public int Height {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    // throw new Exception("Size should be positive.");
+                    value = -value;
+                }
+                height = value;
+            }
+        }
+
+        public int Volume
+        {
+            get
+            {
+                return Width * Length * Height;
+            }
+        }
+
+        // make vars private and set with method
+        // can also make a getLength method
+        /*
+        public void SetLength(int length)
+        {
+            this.length = length;
+        }
+        */
+
+        // CHALLENGE
+        // CREATE A READONLY PROPERTY THAT CALCULATES "FRONTSURFACE" BASED ON HEIGHT AND LENGTH
+        public int FrontSurface { get => Height * Length; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Length is {length} and height is {height} and width is {Width}, so the volume is {Volume}.");
+            Console.WriteLine($"Oh yeah... my Front Surface is {FrontSurface}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Box box = new Box();
+            // box.length = 5;
+            // Set length with setter method
+            // box.SetLength(5);
+
+            // Set length with setter property
+            box.Length = 5;
+            //box.height = 3;
+            //box.width = 4;
+
+            box.Width = 4;
+            box.Height = -3;
+
+            box.DisplayInfo();
+            */
+
+            Box box2 = new Box(5, 3, 4);
+            box2.DisplayInfo();
+        }
+    }
+}
