238ac4674eb35d642c647ce3dd2590bd854eb536
stevenremenapp <remenapp@gmail.com>
2019-07-04 22:48:55 -0400

    Start Vue and Go

---
+++ go/mcleod/golang/001-helloWorld/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, this is GO!")
+	foo()
+	fmt.Println("something more")
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
