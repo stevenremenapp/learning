3e37bed179b7dca34614bb72a37982b713f14319
Steven Remenapp <remenapp@gmail.com>
2020-09-21 13:54:45 -0400

    Progress in React course

--- Udemy/neagoie-react/crwn-clothing/src/App.js
+++ Udemy/neagoie-react/crwn-clothing/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Header from './components/header/header.component';
 import { HomePage } from './pages/homepage/homepage.component';
-
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
